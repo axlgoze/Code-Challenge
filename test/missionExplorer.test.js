e1a9432ed142ac6731da46ddfc32fe99f15015bc
Axel Reyes <rbaxel.f@gmail.com>
2022-06-11 16:08:47 -0500

    agregando prueba

--- test/missionExplorer.test.js
+++ test/missionExplorer.test.js
@@ -8,5 +8,9 @@ describe("Esto es una suite de pruebas",()=>{
 		const  myMissionExplorer = new MissionExplorer("Fromm");
 		expect(myMissionExplorer.name).toBe("Fromm");
 	});
+	test('3) lee nombre de objeto de mission explorer', ()=>{
+		const  myMissionExplorer = new MissionExplorer("Gerardo");
+		expect(myMissionExplorer.name).toBeDefined();
+	});
 
 })
