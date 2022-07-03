0a748412a15d3502777e9972f5bd6262149169c9
Axel Reyes <rbaxel.f@gmail.com>
2022-07-02 21:41:20 -0500

    agregando prueba para emails

--- test/services/studentServices.test.js
+++ test/services/studentServices.test.js
@@ -1,5 +1,6 @@
 const Reader=require("./../../app/models/utils/Reader");
-const StudentService=require("./../../app/models/services/studentService")
+const StudentService=require("./../../app/models/services/studentService");
+const CertificationService=require("./../../app/models/services/CertificationService");
 describe("Test Suite for student services", () =>{
 	test('0) Read json file',()=>{
 		const students = Reader.readJsonFile("students.json");
@@ -10,4 +11,10 @@ describe("Test Suite for student services", () =>{
 		const studentsAmount = StudentService.getStudents(students);
 		expect(studentsAmount).toBe(51);
 	});
+	test("2) get email from students with certificacion", ()=>{
+		const students = Reader.readJsonFile("students.json");
+		const StudentsEmailsWithCertification = CertificationService.haveCertification(students);
+		expect(StudentsEmailsWithCertification).toBe(25);
+	});
+
 });
